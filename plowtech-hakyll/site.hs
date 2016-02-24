789a73a6e3951dce18db66c842b7aace20c13f6b
Scott Murphy <scottmurphy09@gmail.com>
2016-02-23 22:56:30 -0600

    New dependencies and new compiler, also fixed javascript problem

--- plowtech-hakyll/site.hs
+++ plowtech-hakyll/site.hs
@@ -1,17 +1,18 @@
 --------------------------------------------------------------------------------
 {-# LANGUAGE OverloadedStrings #-}
-import           Control.Lens                           hiding (Context)
+import           Control.Lens                           hiding (Context, Level)
 import qualified Data.Attoparsec.Text                   as AttoParsec
 import           Data.Maybe                             (fromMaybe)
-import           Data.Monoid                            (mappend)
+import           Data.Monoid                            (mappend, (<>))
 import           Data.OrgMode.Parse.Attoparsec.Document
+import           Data.OrgMode.Parse.Types
 import           Data.OrgMode.Parse.Types
 import           Data.Text                              (Text)
 import qualified Data.Text                              as Text
 import qualified Data.Text.IO                           as Text
+import qualified Data.Vector                            as Vector
 import           Debug.Trace
 import           Hakyll
-
 
 --------------------------------------------------------------------------------
 main :: IO ()
@@ -22,8 +23,6 @@ main = hakyll $ do
         match "assets/img/carousel/*" $ do
             route   idRoute
             compile copyFileCompiler
-
-
 
 
 
@@ -45,8 +44,6 @@ main = hakyll $ do
     match "assets/css/*" $ do
         route   idRoute
         compile compressCssCompiler
-
-
 
 
 
@@ -73,10 +70,7 @@ main = hakyll $ do
 
     match "products/*" $ do
         route $ setExtension "html"
-        compile $ pandocCompiler
-            >>= loadAndApplyTemplate "templates/post.html"    postCtx
-            >>= loadAndApplyTemplate "templates/default.html" postCtx
-            >>= relativizeUrls
+        compile $ productCompiler  >>= relativizeUrls
 
 
 
@@ -98,7 +92,7 @@ main = hakyll $ do
     match "index.html" $ do
         route idRoute
         compile $ do
-            posts <- recentFirst =<< loadAll "posts/*"
+
             packages <- do
                packages <- loadAll "frontpage/packages-*.md"
                loadAndApplyTemplate "templates/frontpage/package-points.html" defaultContext `traverse` packages
@@ -106,7 +100,6 @@ main = hakyll $ do
                       getMatches "assets/img/carousel/*" :: Compiler [Item Identifier]
 --            images <- recentFirst =<< loadAll "assets/img/*"
             let indexCtx =
-                    listField "posts" postCtx (return posts) `mappend`
                     constField "title" "Home"                `mappend`
                     listField "packages" defaultContext (return packages) `mappend`
                     listField "images" filePathCtx (traverse filepathGrabber images) `mappend`
@@ -153,15 +146,24 @@ filepathGrabber ident = toString
 lDocumentHeadings :: Lens' Document [Heading]
 lDocumentHeadings = lens documentHeadings (\d n -> d {documentHeadings=n} )
 
+lItemBody :: Lens' (Item a) a
+lItemBody = lens itemBody (\i v -> i{itemBody= v})
 
 lHeadingTitle :: Lens' Heading Text
 lHeadingTitle = lens title (\h t -> h {title = t})
 
+lHeadingLevel :: Lens' Heading Level
+lHeadingLevel = lens level (\h t -> h {level = t})
+
+
+lHeadingSection :: Lens' Heading Section
+lHeadingSection = lens section (\h t -> h {section = t})
 
 --------------------------------------------------
 -- Org mode Parsing
 --------------------------------------------------
 
+parseAsOrgMode :: Text -> Either String Document
 parseAsOrgMode txt = returnTemplate
   where
     returnTemplate = parseDocumentWithKeyWords txt
@@ -173,11 +175,106 @@ parseAsOrgMode txt = returnTemplate
 retrieveDocumentTitle :: Document -> Text
 retrieveDocumentTitle doc = doc ^? lDocumentHeadings . folded . lHeadingTitle & fromMaybe ""
 
-orgmodeCompiler :: Compiler (Item String)
-orgmodeCompiler = do
-  itemBody' <- getResourceBody
-  let
-    body = Text.pack . itemBody $ itemBody'
-    title = Text.unpack $ either (const "") retrieveDocumentTitle $ parseAsOrgMode body
-    orgContext = constField "title" title
-  applyAsTemplate orgContext itemBody'
+
+--    title = Text.unpack $ either (const "") retrieveDocumentTitle $ parseAsOrgMode body
+--    orgContext = constField "product-title" title
+--  applyAsTemplate orgContext itemBody'
+--  rslt <- pandocCompiler
+--  return (orgContext , rslt)
+
+isoLevel :: Iso' Level Int
+isoLevel = iso (\(Level a) -> a) Level
+
+
+
+
+
+
+
+
+
+--------------------------------------------------
+-- | Product Definition
+--------------------------------------------------
+data ProductPage = ProductPage {
+                   _productTitle       :: {-# UNPACK  #-} !Text
+                 , _productImage       :: {-# UNPACK  #-} !Text
+                 , _productDescription :: {-# UNPACK  #-} !Text
+               }
+
+productTitle :: Lens' ProductPage Text
+productTitle = lens _productTitle (\p v -> p{_productTitle = v})
+
+productImage :: Lens' ProductPage Text
+productImage = lens _productImage (\p v -> p{_productImage = v})
+
+productDescription :: Lens' ProductPage Text
+productDescription = lens _productDescription (\p v -> p{_productDescription = v})
+
+
+splitDocumentation :: Document -> ProductPage
+splitDocumentation doc = ProductPage  (titlePortion doc )
+                                      (imagePortion doc )
+                                      (descriptionPortion doc )
+   where
+     titlePortion d = retrieveDocumentTitle d
+     imagePortion d = d ^? lDocumentHeadings .folded . lHeadingSection  <&> sectionParagraph & fromMaybe ""
+     descriptionPortion d = d & lDocumentHeadings %~ drop 1 & orgModePrinter
+
+
+
+
+
+
+
+productCompiler :: Compiler (Item String)
+productCompiler = do
+  body <- getResourceBody :: Compiler (Item String)
+  let maybeDoc =  (body ^. lItemBody & parseAsOrgMode . Text.pack) ^? _Right
+  case maybeDoc of
+      Nothing -> return $ body
+      (Just doc) -> (templateApplication $ (const (splitDocumentation doc)) <$> body)
+   where
+      productContext :: Context ProductPage
+      productContext = field "product-title"  (\ip -> (return . itemBody . fmap (Text.unpack . _productTitle)) ip) <>
+                       field "product-image"  (\ip -> (fmap itemBody. renderPandoc . fmap (Text.unpack . _productImage)) ip) <>
+                       field "product-description" (\ip -> (fmap itemBody. renderPandoc . fmap (Text.unpack . _productDescription)) ip)
+      templateApplication body = loadAndApplyTemplate "templates/product.html" productContext  body
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- | Use this to print back multiple Documents after parsing and sorting
+
+-- | Pretty Printer for headings and body
+orgModePrinter :: Document -> Text
+orgModePrinter doc =  ( printHeadingVector . Vector.fromList . documentHeadings) doc
+
+
+printHeadingVector :: Vector.Vector Heading -> Text
+printHeadingVector vec = Vector.foldr' printDocument "" vec
+  where
+    printDocument heading txt = printHeader heading <>
+                                (printHeadingVector . Vector.fromList. subHeadings) heading <>
+                                txt
+    printHeader heading  = levelPrinter heading <>" " <> title heading <> "\n" <> (sectionParagraph . section) heading
+
+
+
+    levelPrinter heading = Text.replicate (heading^.lHeadingLevel.isoLevel) "*"
+
+
+testPrettyPrinter = do
+  (Right doc) <- parseAsOrgMode <$> Text.readFile "example-document.org"
+  return $ orgModePrinter doc
+
+
